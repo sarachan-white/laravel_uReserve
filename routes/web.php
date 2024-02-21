9dc4051a96c5f708106930b539757eed9b242565
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-02-21 13:09:04 +0900

    fin

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware('can:user-higher')
         Route::post('/{id}', [ReservationController::class, 'reserve'])->name('events.reserve');
     });
 
-Route::middleware('auth')->get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
+Route::get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
 
 
 Route::controller(LivewireTestController::class)
